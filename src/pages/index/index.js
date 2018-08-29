f1f8a3a083511fb7ff81c1d62340511e5f7001e0
rollingrotator <513011631@qq.com>
2018-08-29 18:41:02 +0800

    获取后台数据渲染到主页,添加了底部导航组件

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,2 +1,57 @@
-import 'css/common.css'
-import './index.css'
+import 'css/common.css';
+import './index.css';
+import Vue from 'vue';
+import url from 'js/api.js';
+import axios from 'axios';
+
+import { InfiniteScroll } from 'mint-ui';
+Vue.use(InfiniteScroll);
+
+import Foot from 'components/Foot.vue';
+
+
+
+let app=new Vue({
+    el:'#app',
+    data:{
+        lists:null,
+        pageNum:1,
+        pageSize:6,
+        loading:false,
+        allLoaded:false,
+    },
+    created(){
+        this.getLists()
+    },  
+    methods:{
+        getLists(){
+            if(this.allLoaded)return
+            //是否在加载中
+            this.loading=true
+            axios.post(url.hotLists, {
+                pageNum: this.pageNum,
+                pageSize: this.pageSize,
+            }).then((res) => {
+                let curLists=res.data.lists
+                if(curLists.length<this.pageSize){
+                    //判断所有数据是否加载完毕
+                    this.allLoaded=true
+                }
+                if(this.lists){
+                    this.lists=this.lists.concat(curLists)
+                }else{
+                    //第一次请求数据
+                    this.lists = curLists    
+                }
+                
+                console.log(res, this.lists)
+                this.loading=false
+                this.pageNum++
+            }).catch((err) => {});
+        }
+    },
+    components:{
+        Foot
+    }
+    
+})
