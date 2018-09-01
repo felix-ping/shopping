58515b2a949daa5d9d6c8528751eb9971ebc15fb
rollingrotator <513011631@qq.com>
2018-09-01 14:52:33 +0800

    scroll

--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 import url from 'js/api.js';
 import qs from 'qs';
 import mixin from 'js/mixin.js'
+import Velocity from 'velocity-animate';
 
 let {keyword , id}=qs.parse(location.search.substr(1))
 new Vue({
@@ -12,6 +13,7 @@ new Vue({
     data:{
         searchList:null,
         keyword,
+        isShow:false,
     },
     created(){
         this.getSearchLists() 
@@ -21,7 +23,18 @@ new Vue({
             axios.post(url.searchList,{keyword,id}).then(res=>{
                 this.searchList=res.data.lists
             })
-        } 
+        },
+        moveEvent(){
+            if(document.documentElement.scrollTop>100){
+                this.isShow=true
+            }else{
+                this.isShow=false
+            }
+        },
+        goToTop(){
+            //Velocity 库的使用
+            window.scrollTo(0,0)
+        }
     },
     mixins:[mixin]
     
