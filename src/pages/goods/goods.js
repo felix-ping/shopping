366d3faeca0d57768291bfa2278227eb1d099d12
rollingrotator <513011631@qq.com>
2018-09-05 18:53:58 +0800

    skuNum

--- src/pages/goods/goods.js
+++ src/pages/goods/goods.js
@@ -1,14 +1,15 @@
 import './goods_common.css';
-import './goods_custom.css'
-import './goods.css'
-import './goods_theme.css'
-import './goods_mars.css'
-import './goods_sku.css'
+import './goods_custom.css';
+import './goods.css';
+import './goods_theme.css';
+import './goods_mars.css';
+import './goods_sku.css';
+import './goods_transition.css'
 
 import Vue from 'vue';
 import axios from 'axios';
 import url from 'js/api.js';
-import mixin from 'js/mixin.js'
+import mixin from 'js/mixin.js';
 import qs from 'qs';
 import Swiper from 'components/Swiper.vue';
 
@@ -21,6 +22,11 @@ new Vue({
         tabDetails:['商品详情','本店成交'],
         deal:null,
         bannerLists:null,
+        skuType:1,
+        showSku:false,
+        skuNum:1,
+        totalSku:10,
+
     },
     created(){
         this.getDetails()
@@ -48,6 +54,39 @@ new Vue({
             axios.post(url.deal).then(res=>{
                 this.deal=res.data.data.lists
             })
+        },
+        chooseSku(type){
+            this.skuType=type
+            this.showSku=true
+        },
+        changeSkuNum(num){
+            if(num<0&&this.skuNum===1){
+                this.skuNum=1
+            }else{
+                this.skuNum+=num
+                if(this.skuNum>this.totalSku){
+                    this.skuNum=this.totalSku
+                }
+            }
+            
+            
+        },
+    },
+    watch:{
+        showSku(val,oldVal){
+           
+            if(val){
+                document.body.style.overflow='hidden'
+                document.querySelector('html').style.overflow='hidden'
+                document.body.style.height='100%'
+                document.querySelector('html').style.height='100%'
+
+            }else{
+                document.body.style.overflow='auto';
+                document.querySelector('html').style.overflow='auto';
+                document.body.style.height='auto';
+                document.querySelector('html').style.height='auto';
+            }
         }
     },
     components:{
